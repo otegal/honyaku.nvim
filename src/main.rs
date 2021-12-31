f372f45034fa3821c911563e4602b20fbb8192f0
otegal <gyouzakuuka@gmail.com>
2021-12-31 18:56:21 +0900

    APIキーを環境変数で管理できるようにdotenvを導入した。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+
+fn transrate_api_url() -> Result<String, dotenv::Error> {
+    dotenv::var("TRANSRATE_API_URL")
+}
+
+fn main() -> Result<(), dotenv::Error> {
+    dotenv::dotenv().ok();
+    let url = transrate_api_url()?;
+    println!("{}", url);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transrate_api_url() -> Result<(), dotenv::Error> {
+        dotenv::from_filename(".env.example").ok();
+        let url = transrate_api_url()?;
+        assert_eq!(
+            "https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXX/exec",
+            url
+        );
+
+        Ok(())
+    }
 }
